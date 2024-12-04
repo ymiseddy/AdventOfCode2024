57369c106b2d603796b136e59e2dc05064e8d4f1
Shawn Eddy <ymiseddy@gmail.com>
2024-12-04 13:24:42 -0500

    Pregaming Day 5.

---
+++ Day05/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ymiseddy/AdventOfCode2024/shared"
+	"os"
+)
+
+var title string = "Advent of Code 2024, Day 5"
+
+func Puzzle1(lines []string) int64 {
+	total := int64(0)
+	return total
+}
+
+func Puzzle2(lines []string) int64 {
+	total := int64(0)
+	return total
+}
+
+func main() {
+	fmt.Println(title)
+	// Read all text from stdin
+	lines := shared.ReadLinesFromStream(os.Stdin)
+	res1 := Puzzle1(lines)
+	fmt.Println("Puzzle 1 result: ", res1)
+	res2 := Puzzle2(lines)
+	fmt.Println("Puzzle 2 result: ", res2)
+}

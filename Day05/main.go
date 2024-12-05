bf59b89f0104e1237302107629ee32e41772517f
Shawn Eddy <ymiseddy@gmail.com>
2024-12-05 01:43:10 -0500

    Adding day 5 solution.

--- Day05/main.go
+++ Day05/main.go
@@ -2,19 +2,187 @@ package main
 
 import (
 	"fmt"
-	"github.com/ymiseddy/AdventOfCode2024/shared"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/ymiseddy/AdventOfCode2024/shared"
 )
 
 var title string = "Advent of Code 2024, Day 5"
 
 func Puzzle1(lines []string) int64 {
 	total := int64(0)
+	// Map of int64 to int64
+	forward := make(map[int64][]int64)
+	backward := make(map[int64][]int64)
+	next := 0
+	for c, line := range lines {
+		if line == "" {
+			next = c
+			break
+		}
+		// split on bars
+		split := strings.Split(line, "|")
+		if len(split) != 2 {
+			continue
+		}
+		// convert to int64
+		l1, _ := strconv.ParseInt(split[0], 10, 64)
+		l2, _ := strconv.ParseInt(split[1], 10, 64)
+		// Check if l1 is in map
+		_, ok := forward[l1]
+		if !ok {
+			forward[l1] = []int64{l2}
+		} else {
+			forward[l1] = append(forward[l1], l2)
+		}
+
+		_, ok = backward[l2]
+		if !ok {
+			backward[l2] = []int64{l1}
+		} else {
+			backward[l2] = append(backward[l2], l1)
+		}
+	}
+	remaining := lines[next+1:]
+	for _, line := range remaining {
+		split := strings.Split(line, ",")
+		seenSet := make(map[int64]struct{})
+
+		// Create a slice of int64
+		arr := make([]int64, len(split))
+		inOrder := true
+		for n, s := range split {
+			i, _ := strconv.ParseInt(s, 10, 64)
+			arr[n] = i
+			rules := forward[i]
+			/*
+				if rules == nil {
+					continue
+				}
+			*/
+			seenSet[i] = struct{}{}
+			for _, r := range rules {
+				if _, ok := seenSet[r]; ok {
+					inOrder = false
+					break
+				}
+			}
+		}
+		if inOrder {
+			// Get middle element
+			midpoint := (len(arr) / 2)
+			mid := arr[midpoint]
+			total += mid
+		} else {
+		}
+	}
 	return total
 }
 
 func Puzzle2(lines []string) int64 {
 	total := int64(0)
+	// Map of int64 to int64
+	forward := make(map[int64][]int64)
+	backward := make(map[int64][]int64)
+	next := 0
+	for c, line := range lines {
+		if line == "" {
+			next = c
+			break
+		}
+		// split on bars
+		split := strings.Split(line, "|")
+		if len(split) != 2 {
+			continue
+		}
+		// convert to int64
+		l1, _ := strconv.ParseInt(split[0], 10, 64)
+		l2, _ := strconv.ParseInt(split[1], 10, 64)
+		// Check if l1 is in map
+		_, ok := forward[l1]
+		if !ok {
+			forward[l1] = []int64{l2}
+		} else {
+			forward[l1] = append(forward[l1], l2)
+		}
+
+		_, ok = backward[l2]
+		if !ok {
+			backward[l2] = []int64{l1}
+		} else {
+			backward[l2] = append(backward[l2], l1)
+		}
+	}
+	remaining := lines[next+1:]
+	for _, line := range remaining {
+		split := strings.Split(line, ",")
+		seenSet := make(map[int64]struct{})
+		valueSet := make(map[int64]struct{})
+
+		// Create a slice of int64
+		arr := make([]int64, len(split))
+		inOrder := true
+		for n, s := range split {
+			i, _ := strconv.ParseInt(s, 10, 64)
+			arr[n] = i
+			valueSet[i] = struct{}{}
+			rules := forward[i]
+			seenSet[i] = struct{}{}
+			for _, r := range rules {
+				if _, ok := seenSet[r]; ok {
+					inOrder = false
+					break
+				}
+			}
+		}
+		// Ignoring inorder this time.
+		if inOrder {
+			continue
+		}
+
+		// Put the list in order using rules
+		orderedByRules := []int64{}
+		seenSet = make(map[int64]struct{})
+		for {
+			// Find the first value that is not in seenSet
+			var value int64 = -1
+			for _, i := range arr {
+				if _, ok := seenSet[i]; !ok {
+					value = i
+					continue
+				}
+			}
+			if value == -1 {
+				break
+			}
+			for {
+				swapped := false
+				// What must come before value
+				rules := backward[value]
+				// Check to see if any values in arr is in rules
+				for _, r := range rules {
+					if _, ok := valueSet[r]; ok {
+						swapped = true
+						value = r
+						break
+					}
+				}
+				// If nothing was swapped, we found the item with highest precedence
+				if !swapped {
+					break
+				}
+			}
+
+			seenSet[value] = struct{}{}
+			delete(valueSet, value)
+			orderedByRules = append(orderedByRules, value)
+		}
+
+		midpoint := (len(orderedByRules) / 2)
+		total += orderedByRules[midpoint]
+	}
 	return total
 }
 

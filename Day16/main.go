3fe8c9167f286c1b40ce31646a90f2be860cfa09
Shawn Eddy <ymiseddy@gmail.com>
2024-12-15 17:16:09 -0500

    Pre-gaming for Day 16

---
+++ Day16/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ymiseddy/AdventOfCode2024/shared"
+)
+
+var title string = "Advent of Code 2024, Day 16"
+
+var debug = false
+
+func Puzzle1(lines []string) int64 {
+	var total int64 = 0
+	return total
+}
+
+func Puzzle2(lines []string) int64 {
+	var total int64 = 0
+	return total
+}
+
+func main() {
+	fmt.Println(title)
+	// Read all text from stdin
+	lines := shared.ReadLinesFromStream(os.Stdin)
+	res1 := Puzzle1(lines)
+	fmt.Println("Puzzle 1 result: ", res1)
+	res2 := Puzzle2(lines)
+	fmt.Println("Puzzle 2 result: ", res2)
+}

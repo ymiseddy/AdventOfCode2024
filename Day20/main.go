9809f9cf650f70b7e6490d66c00da1ffaf9fa6ea
Shawn Eddy <ymiseddy@gmail.com>
2024-12-20 02:26:15 -0500

    Day 20 solution.

---
+++ Day20/main.go
@@ -0,0 +1,205 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/ymiseddy/AdventOfCode2024/shared"
+)
+
+var title string = "Advent of Code 2024, Day "
+
+func ParseInput(lines []string) (shared.Coord, shared.Coord, [][]rune) {
+	grid := make([][]rune, len(lines))
+	start := shared.Coord{X: -1, Y: -1}
+	end := shared.Coord{X: -1, Y: -1}
+	for y, line := range lines {
+		grid[y] = make([]rune, len(line))
+		for x, char := range line {
+			if char == 'S' {
+				start = shared.Coord{X: x, Y: y}
+			}
+			if char == 'E' {
+				end = shared.Coord{X: x, Y: y}
+			}
+			grid[y][x] = char
+		}
+	}
+
+	return start, end, grid
+}
+
+func LeastCostPath(start shared.Coord, end shared.Coord, grid [][]rune, debug bool) []shared.Coord {
+	var total int = 0
+	visited := make(map[shared.Coord]struct{})
+	visitedList := []shared.Coord{start}
+	queue := []shared.Coord{start}
+	parent := make(map[shared.Coord]shared.Coord)
+	parent[start] = shared.Coord{X: -1, Y: -1}
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		if current == end {
+			total++
+			continue
+		}
+
+		for _, adjacent := range current.Adjacencies() {
+			if adjacent.X < 0 || adjacent.Y < 0 || adjacent.X >= len(grid) || adjacent.Y >= len(grid[0]) {
+				continue
+			}
+			if adjacent == end {
+				parent[end] = current
+				path := []shared.Coord{end}
+				path = append(path, current)
+				walk := current
+				for {
+					walk = parent[walk]
+					path = append(path, walk)
+					if walk == start {
+						break
+					}
+				}
+				return path
+			}
+
+			if grid[adjacent.Y][adjacent.X] == '#' {
+				continue
+			}
+
+			if _, ok := visited[adjacent]; !ok {
+				visited[adjacent] = struct{}{}
+				visitedList = append(visitedList, adjacent)
+				parent[adjacent] = current
+				queue = append(queue, adjacent)
+			}
+		}
+	}
+
+	shared.ShowGridStep(grid, debug, nil, 0)
+	return nil
+}
+
+func FindCheats(path []shared.Coord, grid [][]rune, distances map[shared.Coord]int, debug bool) int {
+	// Wrong 2528
+	countCheats := 0
+	cheatMap := make(map[int]int)
+	for _, p := range path {
+		for dir, adjacent := range p.Adjacencies() {
+			if adjacent.X < 0 || adjacent.Y < 0 || adjacent.X >= len(grid) || adjacent.Y >= len(grid[0]) {
+				continue
+			}
+			if grid[adjacent.Y][adjacent.X] == '#' {
+				// Check to see if the path is on the other side of the wall
+				next := adjacent.Move(dir)
+				ndist, ok := distances[next]
+				if !ok {
+					continue
+				}
+				delta := (ndist - (distances[p] + 2))
+				if delta >= 100 {
+					if _, ok := cheatMap[delta]; !ok {
+						cheatMap[delta] = 1
+					} else {
+						cheatMap[delta]++
+					}
+					countCheats++
+				}
+
+			}
+		}
+	}
+	return countCheats
+}
+
+func Puzzle1(lines []string) int {
+	total := 0
+	start, end, grid := ParseInput(lines)
+	lcp := LeastCostPath(start, end, grid, true)
+	lcpDistance := map[shared.Coord]int{}
+	reversedPath := []shared.Coord{}
+	for n, p := range lcp {
+		lcpDistance[p] = n
+		reversedPath = append(reversedPath, p)
+	}
+	total = FindCheats(reversedPath, grid, lcpDistance, false)
+	return total
+}
+
+type StartEnd struct {
+	Start shared.Coord
+	End   shared.Coord
+}
+
+func FindCheats2(path []shared.Coord, grid [][]rune, distances map[shared.Coord]int, cheatCount int) int {
+
+	cheatMap := make(map[StartEnd]int)
+	for _, p := range path {
+		pEndDistance := distances[p]
+		for _, k := range path {
+			kEndDistance := distances[k]
+			if p == k {
+				continue
+			}
+			distance := shared.ManhattanDistance(p, k)
+			if distance > cheatCount {
+				continue
+			}
+
+			delta := (kEndDistance - (pEndDistance + distance))
+			if delta >= 100 {
+				cheatMap[StartEnd{p, k}] = delta
+			}
+		}
+	}
+
+	// Just so we can print this out to compare with samples.
+	distancesSavedCount := map[int]int{}
+	distancesSavedList := []int{}
+	for _, v := range cheatMap {
+		distance := v
+		if _, ok := distancesSavedCount[distance]; !ok {
+			distancesSavedList = append(distancesSavedList, distance)
+			distancesSavedCount[distance] = 1
+		} else {
+			distancesSavedCount[distance]++
+		}
+	}
+
+	// Sort distancesSavedList
+	sort.Ints(distancesSavedList)
+
+	for _, s := range distancesSavedList {
+		fmt.Printf("Cheat %d: %d\n", s, distancesSavedCount[s])
+	}
+
+	return len(cheatMap)
+}
+
+func Puzzle2(lines []string) int {
+	total := 0
+	start, end, grid := ParseInput(lines)
+	lcp := LeastCostPath(start, end, grid, true)
+	lcpDistance := map[shared.Coord]int{}
+	reversedPath := []shared.Coord{}
+	for n, p := range lcp {
+		lcpDistance[p] = n
+		reversedPath = append(reversedPath, p)
+	}
+	total = FindCheats2(reversedPath, grid, lcpDistance, 20)
+	return total
+}
+
+func main() {
+	fmt.Println(title)
+	// Read all text from stdin
+
+	lines := shared.ReadLinesFromStream(os.Stdin)
+
+	res1 := Puzzle1(lines)
+	fmt.Println("Puzzle 1 result: ", res1)
+	res2 := Puzzle2(lines)
+	fmt.Println("Puzzle 2 result: ", res2)
+}
--- shared/shared.go
+++ shared/shared.go
@@ -3,6 +3,7 @@ package shared
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -76,6 +77,55 @@ func Clear() {
 type Coord struct {
 	X int
 	Y int
+}
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func ManhattanDistance(c1 Coord, c2 Coord) int {
+	return Abs(c1.X-c2.X) + Abs(c1.Y-c2.Y)
+}
+
+func EuclideanDistance(c1 Coord, c2 Coord) int {
+	return int(math.Sqrt(float64(c1.X-c2.X)*float64(c1.X-c2.X) + float64(c1.Y-c2.Y)*float64(c1.Y-c2.Y)))
+}
+
+func (c Coord) Move(dir int) Coord {
+	dx, dy := CardinalDirections[dir][0], CardinalDirections[dir][1]
+	c.X += dx
+	c.Y += dy
+	return c
+}
+
+func (c Coord) Adjacencies() []Coord {
+	var result []Coord
+	for _, dir := range []int{0, 1, 2, 3} {
+		result = append(result, c.Move(dir))
+	}
+	return result
+}
+
+var CardinalDirections = [][]int{
+	// North
+	{0, -1},
+	// East
+	{1, 0},
+	// South
+	{0, 1},
+	// West
+	{-1, 0},
+}
+
+func Clockwise(dir int) int {
+	return (dir + 1) % 4
+}
+
+func CounterClockwise(dir int) int {
+	return (dir + 3) % 4
 }
 
 func ShowGridStep(grid [][]rune, debug bool, positions []Coord, sleepTime int) {
